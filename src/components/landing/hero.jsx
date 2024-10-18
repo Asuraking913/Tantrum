824f1b3b70dfbc2a860e3898bd130c92490c62d0
Israel Shedrack <israelshedrack913@gmail.com>
2024-10-18 17:51:08 +0100

    update: created responsiveness for landing page, creating stories page

--- src/components/landing/hero.jsx
+++ src/components/landing/hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
     <section className='h-screen rounded-b-[15px] sm:flex-row flex-col justify-center overflow-hidden bg-[--black] flex items-center sm:justify-center text-[--white] px-[1em] sm:px-[--pdx] linear relative'>
         <div  className='text-center sm:w-[80%] flex flex-col gap-[2em] sm:gap-[15px] w-full'>
             <p className='pop text-[--accent] font-bold capitalize text-[1.2rem]'>Throw a Tantrum!</p>
-            <h1 className='sm:text-[6rem] text-[1.9rem] urba font-semibold capitalize line'>Everyone has a story. what's yours?</h1>
+            <h1 className='sm:text-[6rem] text-[2.2rem] urba font-semibold capitalize line'>Everyone has a story. what's yours?</h1>
             <p className='urba text-[1.5rem] sm:text-[1.8rem] opacity-80'>Vent and Share Your Emotions, Don't keep it in!!</p>
             <div className='sm:block inline'>
                 <Button text={"Share your story"} inverse={false}/>
--- src/components/primary/nav.jsx
+++ src/components/primary/nav.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import icon from "../../assets/icon.png"
-import { FaBars, FaGithub } from 'react-icons/fa'
+import { FaBars, FaGithub, FaTimes } from 'react-icons/fa'
 import { FaQuestion } from 'react-icons/fa'
 import { useLocation, useNavigate } from 'react-router-dom'
 
@@ -16,38 +16,39 @@ function Nav() {
 
 
   return (
-    <header className='z-[1] fixed top-0 w-full p-[5px] sm:p-[10px] px-[1em] sm:px-[--pdx] items-center flex justify-between'>
+    <header className={`z-[1] sm:h-auto ${nav && "h-screen"} fixed top-0 w-full p-[5px] sm:p-[10px] px-[1em] sm:px-[--pdx] items-center flex justify-between`}>
         <div className='absolute left-0 z-[-1] backdrop-blur-lg h-full w-full'>
 
         </div>
-        <div className='flex items-center sm:flex-row flex-col text-[--white]'>
+        <div className='flex sm:relative fixed top-0 left-0 p-[10px] sm:p-0  items-center sm:flex-row flex-col text-[--white]'>
             <img className='w-[30px] sm:w-[40px]' src={icon} alt="" />
             <a href="#" className='pop sm:text-xl text-[0.95rem]'>Tantrum</a>
         </div>
 
-        {!nav && <FaBars onClick={() => setNav(!nav)} size={30} className='sm:hidden' color='#E9602C'/>}
+        {!nav && <FaBars onClick={() => setNav(!nav)} size={30} className='sm:hidden z-[20] fixed right-0 top-0 m-[15px]' color='#E9602C'/>}
  
-        <ul className={`urba px-[--pdx] gap-[1em] text-[1.1rem] text-[--white] sm:flex ${!nav && "hidden"}`}>
-            <li className='relative'>
+        <ul className={`urba px-[--pdx] gap-[1em] text-[1.1rem] text-[--white] flex-col sm:w-auto w-full items-center sm:items-end  sm:flex-row sm:flex flex ${!nav && "hidden"}`}>
+            <FaTimes className='absolute bottom-[20%] animate-pulse sm:hidden block' size={50} onClick={() => setNav(!nav)} color='var(--accent)'/>
+            <li className='p-[10px] sm:p-0 sm:shadow-none shadow-sm shadow-[--white] sm:w-auto w-[200px] flex sm:block sm:justify-normal justify-center sm:rounded-none rounded-[2em] relative'>
                 <button onClick={() => navigate("/")} href="#">
                     Home
                 </button>
-                <div className='absolute w-full h-[20%] duration-[0.5s]  rounded-[4px] bg-[--accent]'>
+                {!nav && <div className='absolute w-full h-[20%] duration-[0.5s]  rounded-[4px] bg-[--accent]'>
 
-                </div>
+                </div>}
             </li>
-            <li>
+            <li className='p-[10px] sm:p-0 sm:shadow-none shadow-sm shadow-[--white] sm:w-auto w-[200px] flex sm:block sm:justify-normal justify-center sm:rounded-none rounded-[2em] '>
                 <button onClick={() => navigate("/stories")} href="#">
                     Stories
                 </button>
             </li>
-            <li>
+            <li className='p-[10px] sm:p-0 sm:shadow-none shadow-sm shadow-[--white] sm:w-auto w-[200px] flex sm:block sm:justify-normal justify-center sm:rounded-none rounded-[2em] '>
                 <button href="#" className='flex items-center gap-[10px]'>
                     <span>About</span> 
                 </button>
                 
             </li>
-            <li>
+            <li className='p-[10px] sm:p-0 sm:shadow-none shadow-sm shadow-[--white] sm:w-auto w-[200px] flex sm:block sm:justify-normal justify-center sm:rounded-none rounded-[2em] '>
                 <button href="#" className='flex items-center gap-[10px]'>
                     <span>Github</span> 
                     <span><FaGithub /></span>
